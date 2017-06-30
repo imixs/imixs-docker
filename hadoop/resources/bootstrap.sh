2e1a4d66c0a540e2054975ed7587cd0147f2f0bf
Ralph Soika <ralph.soika@imixs.com>
2017-06-30 18:10:23 +0200

    some adjustments

--- hadoop/resources/bootstrap.sh
+++ hadoop/resources/bootstrap.sh
@@ -3,10 +3,22 @@
 # start ssh demon 
 /etc/init.d/ssh start
 
+# Format HDFS....?
+if [ ! -f /data/hdfs/runonce.lock ]; then
+    if [ ! -d /data/hdfs/namenode ]; then
+      touch /data/hdfs/runonce.lock
+      echo "NO DATA IN /data/hdfs/namenode"
+      echo "FORMATTING NAMENODE"
+      $HADOOP_HOME/bin/hdfs namenode -format || { echo 'FORMATTING FAILED' ; exit 1; }
+      chown -R hduser:hadoop /data/hdfs || { echo 'CHOWN FAILED' ; exit 1; }
+    fi
+fi
+
+
 # start hadoop DFS
 echo "Starting Hadoop DFS...."
-/opt/hadoop/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-dfs.sh
 
 echo "Hadoop Startup completed" 
 # keep running in the foreground
-tail -f /opt/hadoop/logs/*
+tail -f $HADOOP_HOME/logs/*
