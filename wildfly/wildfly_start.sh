a4777bbdb316edb15a4e05172e53726c4f0186c6
Ralph Soika <ralph.soika@imixs.com>
2019-03-13 08:48:54 +0100

    fixed
    issue #34

--- wildfly/wildfly_start.sh
+++ wildfly/wildfly_start.sh
@@ -16,10 +16,10 @@ fi
 
 # test for starting in debug mode...
 if [ "$DEBUG" == "true" ]; then    
-echo "starting wildfly in debug mode..."
-exec /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 --debug --server-config=standalone.xml
+  echo "starting wildfly in debug mode..."
+  exec /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 --debug *:8787 --server-config=standalone.xml
 else
-echo "starting wildfly..."
-exec /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
+  echo "starting wildfly..."
+  exec /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 fi;
 
